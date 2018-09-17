65b2d32b3a10e91974ea5229e86830c2aaf9c156
Matt Fewer <mattfewer@hotmail.com>
2018-09-17 10:32:39 +0200

    first commit

---
+++ basic.php
@@ -0,0 +1,7 @@
+<?php
+    // 1. Write a PHP script to get the PHP version and configuration information.
+    echo "PHP version: " . phpversion();
+    // echo "Configuration info: " . phpinfo(INFO_CONFIGURATION);
+
+    // can also just run phpinfo();
+?>
